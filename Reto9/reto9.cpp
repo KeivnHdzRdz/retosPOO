2c9ab1f2114b76b1a4aa0302d7c2e9ce59cecabd
KevinHdzRdz <KevinHdzRdz88@gmail.com>
2020-03-21 01:06:10 +0000

    Termine Reto 7 y Reto 10

---
+++ Reto9/reto9.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int solution(vector<int> A);
+
+    for(int i = 0; i < A.size(); i++){
+        for()
+    }
+
+
+int main(){
+    int resultado;
+    vector<int> A={2,4,6};
+    resultado = solution (A);
+
+        printf("\nResult:%d",resultado);
+}
